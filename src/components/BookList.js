f6bb5b05f1b1f4d2e3ebb8ddf3cd1c442f25b822
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-22 22:18:23 +0530

    Added and used useContext hook and replaced class with functional based components

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,25 +1,22 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends Component {
-  static contextType = ThemeContext; // this can only be used in class based components.
-  // if we want to convert it into function based component then we have to use useContext hook.
-  render() {
-    const { isLight, light, dark } = this.context;
-    const theme = isLight ? light : dark;
-    return (
-      <div
-        className="book-list"
-        style={{ color: theme.text, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>The Immortals of Meluha</li>
-          <li style={{ background: theme.ui }}>The Secret of Nagas</li>
-          <li style={{ background: theme.ui }}>The Oath of Vayuputras</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const BookList = () => {
+  const { isLight, light, dark } = useContext(ThemeContext);
+  const theme = isLight ? light : dark;
+
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.text, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>The Immortals of Meluha</li>
+        <li style={{ background: theme.ui }}>The Secret of Nagas</li>
+        <li style={{ background: theme.ui }}>The Oath of Vayuputras</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,59 +1,25 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-// class Navbar extends Component {
-//   // After wrapping, calling ThemeContext here via React property contextType
-//   static contextType = ThemeContext;
-//   render() {
-//     console.log(this.context); // we can access state now using this.context
-//     const { isLight, light, dark } = this.context;
-//     const theme = isLight ? light : dark;
-//     return (
-//       <nav style={{ background: theme.ui, color: theme.text }}>
-//         <h1>Context App</h1>
-//         <ul>
-//           <li>Home</li>
-//           <li>About</li>
-//           <li>Contact</li>
-//         </ul>
-//       </nav>
-//     );
-//   }
-// }
+const Navbar = () => {
+  const { isLight, light, dark } = useContext(ThemeContext);
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const theme = isLight ? light : dark;
 
-// We can also access context using Consumer also like this below.
-// - Above approach is good for class based components and for function based components use below approach.
-// - Another benefit of below approach is that we can consume multiple contexts in one component. Cannot be done by above approach.
-class Navbar extends Component {
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {(authContext) => (
-          <ThemeContext.Consumer>
-            {(themeContext) => {
-              const { isAuthenticated, toggleAuth } = authContext;
-              const { isLight, light, dark } = themeContext;
-              const theme = isLight ? light : dark;
-              return (
-                <nav style={{ background: theme.ui, color: theme.text }}>
-                  <h1>Context App</h1>
-                  <div onClick={toggleAuth}>
-                    {isAuthenticated ? "Logged In" : "Logged Out"}
-                  </div>
-                  <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                  </ul>
-                </nav>
-              );
-            }}
-          </ThemeContext.Consumer>
-        )}
-      </AuthContext.Consumer>
-    );
-  }
-}
+  return (
+    <nav style={{ background: theme.ui, color: theme.text }}>
+      <h1>Context App</h1>
+      <div onClick={toggleAuth}>
+        {isAuthenticated ? "Logged In" : "Logged Out"}
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
