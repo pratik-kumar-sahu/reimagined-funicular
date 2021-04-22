19a5d8348d7ad83d3750223f6df7c6671262e0a5
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-22 17:00:29 +0530

    Updating context data using toggle button

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -8,10 +8,17 @@ export class ThemeContextProvider extends Component {
     light: { text: "#555", ui: "#ddd", bg: "#eee" },
     dark: { text: "#ddd", ui: "#333", bg: "#555" },
   };
+
+  toggleTheme = () => {
+    this.setState({ isLight: !this.state.isLight });
+  };
+
   render() {
     return (
       // Provider will wrap up the components in which we want to use the state
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
         {this.props.children}
       </ThemeContext.Provider>
     );
